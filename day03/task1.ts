fb54fcc2debcff998c50e41dbbff41aff1eff5c8
vasser <serhiy.vas@gmail.com>
2024-12-04 00:04:29 -0500

    Day 03, task2

--- day03/task1.ts
+++ day03/task1.ts
@@ -3,22 +3,29 @@ import path from "node:path";
 import readline from "node:readline";
 
 const rl = readline.createInterface({
+  // to make it pass for both task1 and task2
+  // glued all lines in one line
   input: fs.createReadStream(path.join("day03", "input.txt")),
   output: process.stdout,
   terminal: false,
 });
 
 rl.on("line", (line: string) => {
+  // task 1
   const { starts, ends } = detectMulSignals(line);
   calculateResult(line, starts, ends);
+
+  // task 2
+  calculateResultWithInterruptions(line);
 });
 
 rl.on("close", () => {
   console.log("File reading completed.");
-  console.log({ result });
+  console.log({ result, result2 });
 });
 
 let result = 0;
+let result2 = 0;
 
 const detectMulSignals = (
   line: string
@@ -42,6 +49,39 @@ const detectMulSignals = (
   return { starts, ends };
 };
 
+/**
+ * For second task used regex to detect valid mul signals
+ */
+const calculateResultWithInterruptions = (line: string) => {
+  const enabledInstructions: string[] = [];
+
+  const chunks = line.split("don't()") || [line];
+
+  // instructions before the first interruption are always enabled
+  enabledInstructions.push(chunks[0]);
+
+  for (const chunk of chunks.slice(1)) {
+    // instructions after the interruption are enabled if they contain "do()"
+    const index = chunk.indexOf("do()");
+    if (index >= 0) {
+      enabledInstructions.push(chunk.slice(index + 4));
+    }
+  }
+
+  for (const line of enabledInstructions) {
+    const validMuls = line.match(/mul\((\d+),(\d+)\)/g) || [];
+
+    for (const mul of validMuls) {
+      const nums = mul.match(/\d+/g)?.map(Number) || [];
+      result2 += nums[0] * nums[1];
+    }
+  }
+};
+
+/**
+ * For first task used string manipulation with
+ * starts and ends to detect valid mul signals
+ */
 const calculateResult = (line: string, starts: number[], ends: number[]) => {
   const mul = new RegExp(/^mul\(\d+,\d+\)$/);
 
